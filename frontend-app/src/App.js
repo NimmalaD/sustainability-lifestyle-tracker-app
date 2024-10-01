6ac61afd8393da80b86a93b8a3f5e40f7a673409
Dharma Thanishq Nimmala <nimmala.d@northeastern.edu>
2024-10-01 18:28:54 -0400

    frontned code

---
+++ frontend-app/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes, Switch, Link } from "react-router-dom";
+import Home from "./components/home/Home";
+import Login from "./components/login/Login";
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home></Home>} />
+          <Route path="/login" element={<Login></Login>}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
